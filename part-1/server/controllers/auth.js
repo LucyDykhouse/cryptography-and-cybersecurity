7ffcf14f6190d0f94686f98c8a877caac2210882
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-04 14:17:40 -0400

    Finished register method

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,5 +1,7 @@
 const users = []
 
+const bcrypt = require('bcryptjs');
+
 module.exports = {
     login: (req, res) => {
       console.log('Logging In User')
@@ -13,9 +15,23 @@ module.exports = {
       res.status(400).send("User not found.")
     },
     register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+        const {username, email, firstName, lastName, password} = req.body;
+        const salt = bcrypt.genSaltSync();
+        const passwordHash = bcrypt.hashSync(password, salt);
+
+        let userDataToStore = {
+          username,
+          email,
+          firstName,
+          lastName,
+          passwordHash
+        };
+
+        users.push(userDataToStore);
+        let userDataToSend = {...userDataToStore};
+        delete userDataToSend.passwordHash;
+        console.log(userDataToSend);
+        console.log(userDataToStore);
+        res.status(200).send(userDataToSend);
     }
 }
